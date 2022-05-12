c5bc586c350827ba0c378fee1de1a4ac53f88309
Ravi-Teja-Reddy <ravitejareddy.engineer@gmail.com>
2022-05-12 09:19:15 +0530

    Seive of Eratostheses

---
+++ seiveoferatosthenes.cpp
@@ -0,0 +1,32 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ int countPrimes(int n)
+        {
+
+            bool primes[n + 1];
+
+            memset(primes, true, sizeof(primes));
+
+            for (int p = 2; p * p < n; p++)
+            {
+
+                if (primes[p])
+                {
+
+                    for (int i = p * p; i < n; i += p)
+                    {
+                        primes[i] = false;
+                    }
+                }
+            }
+
+            int count = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (primes[i])
+                    count++;
+            }
+
+            return count;
+        }
